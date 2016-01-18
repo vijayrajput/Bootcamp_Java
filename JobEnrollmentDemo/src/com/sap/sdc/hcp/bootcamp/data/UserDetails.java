6c471b47b95085b70f67d10dca36d87f9fc51c41
Vijay Singh Rajput <vijay.singh.rajput@sap.com>
2016-01-18 23:03:54 +0530

    Authorization Impl

---
+++ JobEnrollmentDemo/src/com/sap/sdc/hcp/bootcamp/data/UserDetails.java
@@ -0,0 +1,47 @@
+package com.sap.sdc.hcp.bootcamp.data;
+
+public class UserDetails {
+	
+	String name;
+	String firstName;
+	String lastName;
+	String email;
+	String country;
+	
+	
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastname(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getCountry() {
+		return country;
+	}
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+}
---
+++ JobEnrollmentDemo/src/com/sap/sdc/hcp/bootcamp/web/GetUserAttributes.java
@@ -0,0 +1,76 @@
+package com.sap.sdc.hcp.bootcamp.web;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.google.gson.Gson;
+import com.sap.sdc.hcp.bootcamp.data.UserDetails;
+import com.sap.security.um.service.UserManagementAccessor;
+import com.sap.security.um.user.User;
+import com.sap.security.um.user.UserProvider;
+
+/**
+ * Servlet implementation class GetUserAttributes
+ */
+@WebServlet("/GetUserAttributes")
+public class GetUserAttributes extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public GetUserAttributes() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		if (request.getUserPrincipal() != null) {
+			  try {
+			    // UserProvider provides access to the user storage
+			    UserProvider users = UserManagementAccessor.getUserProvider();
+
+			    // Read the currently logged in user from the user storage
+			    User user = users.getUser(request.getUserPrincipal().getName());
+
+			    
+			    UserDetails ud = new UserDetails();
+			    ud.setName(user.getAttribute("id"));
+			    ud.setFirstName(user.getAttribute("firstName"));
+			    ud.setLastname(user.getAttribute("lastName"));
+			    ud.setEmail(user.getAttribute("email"));
+			    ud.setCountry(user.getAttribute("country"));
+			    
+		
+			    Gson gson = new Gson();
+
+				// convert java object to JSON format,
+				// and returned as JSON formatted string
+				String json = gson.toJson(ud);
+			    response.getWriter().println(json);
+	
+			    
+			  } catch (Exception e) {
+			    // Handle errors
+			  }
+			}
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
