2b4bdd1d19e1669bef38118bc2b7a0d42e7188e1
Vijay Singh Rajput <vijay.rajput@gmail.com>
2016-03-15 12:36:02 +0530

    Update Job.java

--- JobEnrollmentDemo/src/com/sap/sdc/hcp/bootcamp/model/Job.java
+++ JobEnrollmentDemo/src/com/sap/sdc/hcp/bootcamp/model/Job.java
@@ -30,9 +30,9 @@ public class Job implements Serializable {
 	private String DEPARTMENT; 
 	@Column(name = "\"VALID_TILL\"") 
 	private Timestamp VALID_TILL; 
-	@Column(name = "\"CREATED_ON\"") 
+	@Column(name = "\"SYSTEM.CREATED_ON\"") 
 	private Timestamp CREATED_ON; 
-	@Column(name = "\"CREATED_BY\"", nullable = true, length = 10) 
+	@Column(name = "\"SYSTEM.CREATED_BY\"", nullable = true, length = 10) 
 	private String CREATED_BY; 
 	
 	@Transient
